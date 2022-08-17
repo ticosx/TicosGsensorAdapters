2bde55db88ea849a25bcaf58902d9865ba50bfca
ligping <543227995@qq.com>
2022-08-17 20:13:19 +0800

    feat:增加中断信号配置

--- src/TkGsrSc7a20.h
+++ src/TkGsrSc7a20.h
@@ -31,18 +31,33 @@ Copyright (c) 2017 Silan MEMS. All Rights Reserved.
 #define OUT_Y_H_REG     (uint8_t)0X2B
 #define OUT_Z_L_REG     (uint8_t)0X2C
 #define OUT_Z_H_REG     (uint8_t)0X2D
+#define AOI1_SRC_REG    (uint8_t)0X31 //AOI1中断状态位
 
-#define SC7A20_REG2_NUM        4
+#define SC7A20_REG2_NUM        9
 
-const uint8_t sc7a20_init_reg2[12]=
+/*const uint8_t sc7a20_init_reg2[12]=
 {
     //0x2E,0x00,0x00,//
     0x20,0x47,0x00,//使能x,y,z轴，LPen =0正常模式，50Hz,
     0x23,0x18,0x00,//连续更新数据块，大端数据，4G量程，
     0x24,0xC0,0x00,//使能FIFO模式
     0x2E,0x4F,0x00,//FIFO 模式，阈值是F
-};
+};*/
 
+const uint8_t sc7a20_init_reg2[3*SC7A20_REG2_NUM]=
+{
+    //0x2E,0x00,0x00,//
+    0x20,0x4F,0x00,//使能x,y,z轴，LPen =1低功耗模式，50Hz,
+    0x23,0x18,0x00,//连续更新数据块，大端数据，±4G量程，数字低通滤波器使能
+    0x24,0xC0,0x00,//使能FIFO模式，
+    0x2E,0x47,0x00,//FIFO 模式，阈值是7
+    //于中断相关配置
+    0x21,0xC1,0x00,//设置数据高通
+    0x22,0x40,0x00,//使能AOI1中断，AOI1中断在INT1 脚上
+    0x30,0x3F,0x00,//或中断事件，x,y,z高低事件中断
+    0x32,0x64,0x00,//AOI1 阈值为100（此值可以调整）
+    0x33,0x88,0x00,//设置轴的数据需要连续大于阈值 8 次才能产生中断
+};
 /*!
  * @brief The class of gsensor sc7a20
  */
